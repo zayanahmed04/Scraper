e9880770fe5e1045cc80716b9b9ccb769a2e7ed7
hashir684 <157196811+zayanahmed04@users.noreply.github.com>
2026-05-20 02:12:54 +0500

    Update run(school).bat

--- run(school).bat
+++ run(school).bat
@@ -1,8 +1,18 @@
 @echo off
+
 :loop
-echo Starting gallery-dl...
-gallery-dl --cookies "D:\fb_data\cookies.txt" --download-archive "D:\fb_data\archive.txt" -d "D:\fb_data" "https://www.facebook.com/photo/?fbid=1454822136689952&set=pb.100064865372661.-2207520000&setextract"
+
+echo Starting scraper...
+
+call venv\Scripts\activate
+echo venv activated
+
+python -u "D:\Scraper\fb_picture_downloader.py"
+
 echo.
-echo Finished or stopped. Waiting 30 minutes before retrying...
-timeout /t 1800
+echo Finished or stopped.
+echo Waiting 10 seconds before retrying...
+
+timeout /t 10
+
 goto loop
